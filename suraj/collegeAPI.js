41c063af5594ee3aa1078cf7459499c72acce158
sathya-reddy1658 <ktr6304@gmail.com>
2024-02-15 17:57:55 +0530

    college api

---
+++ suraj/collegeAPI.js
@@ -0,0 +1,33 @@
+
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.getElementById("btn");
+btn.addEventListener("click", async () => {
+
+    console.log("CLICK");
+    let text = document.getElementById("txt");
+    let country = text.value;
+    await college(country);
+
+})
+async function college(country) {
+    try {
+        let response = await fetch(url + country);
+        let raw = await response.json();
+
+        show(raw);
+    } catch (e) {
+        console.log("Error ", e);
+        return [];
+    }
+}
+function show(raw) {
+let list = document.getElementById("list");
+list.innerText ="";
+    for (r of raw) {
+        console.log(r.name);
+       let li =  document.createElement("li");
+       li.innerText = r.name;
+       list.appendChild(li);
+    }
+
+}
